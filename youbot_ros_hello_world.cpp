e9ecc256629f17f53f292113c95ad13197d602c9
romanomatthew23 <mmroman2@illinois.edu>
2015-12-02 14:27:08 -0600

    Added Computer Vision Sucka
    Matt's a boss.

--- youbot_ros_hello_world.cpp
+++ youbot_ros_hello_world.cpp
@@ -1,6 +1,8 @@
 //
 // Simple demo program that calls the youBot ROS wrapper
 //
+
+
 #include <math.h>
 #include <dirent.h>
 #include <cstdlib>
@@ -14,9 +16,14 @@
 ros::Publisher platformPublisher;
 ros::Publisher armPublisher;
 ros::Publisher gripperPublisher;
-
+void get_block_locations();
 #include <unistd.h>
 #include <termios.h>
+#define PI 3.1415926
+double x,y,z;
+double z1, y2, z3;
+double new_joints[5];
+double jnt_0,jnt_1,jnt_2,jnt_3,jnt_4;
 
 char getch() {
         char buf = 0;
@@ -38,36 +45,13 @@ char getch() {
         return (buf);
 }
 
-
-void get_block_locations()
-{
-	system("rm -rf /home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc");
-	system("timeout 2 /home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/fakenect-record /home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc > /dev/null");
-	DIR *dir;
-	struct dirent *ent;
-	std::string filename;
-	if ((dir = opendir ("/home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc")) != NULL) {
-	  /* print all the files and directories within directory */
-	  while ((ent = readdir (dir)) != NULL) {
-		if (strstr(ent->d_name,"r")) {	    
-			filename = ent->d_name;
-			break;
-		}
-	  }
-	  closedir (dir);
-	} else {
-	  /* could not open directory */
-	  perror ("");
-	}
-	std::cout << filename << std::endl;
-	std::string path = "/home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc/";
-	std::string filename_path = path.append(filename); //string of filename included with path
-	std::string command = "convert ";
-	std::string output_str = " /home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc/output.png";
-	command = (command.append(filename_path).append(output_str));
-	const char * c = command.c_str();
-	system(c);
-}
+#define TX_MIN -222.0
+#define TX_SIZE -161.0
+#define TY_MIN 75.0
+#define TY_SIZE - 156.0
+#define TZ_PERCH 112
+#define TZ_GRAB 93
+#define TPITCH -2.25
 
 
 // create a brics actuator message with the given joint position values
@@ -146,6 +130,12 @@ void movePlatform() {
 	platformPublisher.publish(twist);
 }
 
+double float_modulus(double value, double mod) {
+	while (value < 0) value += mod;
+	while (value >= mod) value -=mod;
+	return value;
+}
+
 // move arm once up and down
 void moveArm(double joint_0, double joint_1, double joint_2, double joint_3, double joint_4) {
 //void moveArm() {
@@ -163,6 +153,8 @@ void moveArm(double joint_0, double joint_1, double joint_2, double joint_3, dou
 	joint_2 += -2.44;
 	joint_3 += 1.73;
 	joint_4 += 2.95;
+	joint_4 = float_modulus(joint_4,2*PI);
+	joint_0 = float_modulus(joint_0,2*PI);
         //Doubling the moves to see if it works
 	// move arm straight up. values were determined empirically
 	jointvalues[0] = joint_0;
@@ -217,20 +209,43 @@ bool equ(double a, double b) {
 #define d1 147.0
 #define a2 155.0
 #define a3 135.0
-#define PI 3.1415
+
 #define TRAV_SPEED 1.0
-void inverse_kin(double * arr, double x,double y,double z,double y2,double z3) {
+/*void inverse_kin(double * arr, double x,double y,double z,double y2,double z3) {
 	double * new_joints = new double(5);
 	for (int i = 0; i < 5; i++)
 		arr[i] = 0;
 	arr[0] = atan2(y,x);
-	double z1 = arr[0];
+	z1 = arr[0];
 	double o3x = x - d5*cos(z1)*sin(y2);
 	double o3y = y - d5*sin(z1)*sin(y2);
 	double o3z = z - d5*cos(y2);
 	printf("o3: %lf %lf %lf\n",o3x,o3y,o3z);
 	//double r = sqrt((o3x - a1*cos(z1))*(o3x - a1*cos(z1)) + (o3y - a1*sin(z1))*(o3y - a1*sin(z1)));
 	double r = sqrt(o3x*o3x + o3y*o3y) - a1;	
+	double s = o3z - d1;
+	double cosq3 = (r*r+s*s - a2*a2 - a3*a3)/(2*a2*a3);
+	//case 1
+	arr[4] = z3 + PI;
+	arr[2] = atan2(-1.0*sqrt(1 - cosq3*cosq3),cosq3);
+	arr[1] = atan2(s,r) - atan2(a3*sin(arr[2]),a2+a3*cos(arr[2]));
+	arr[3] = PI/2.0 - y2 - arr[1] - arr[2];
+}
+*/
+void inverse_kin(double * arr, double x,double y,double z,double y2,double z3) {
+	double * new_joints = new double(5);
+	for (int i = 0; i < 5; i++)
+		arr[i] = 0;
+	arr[0] = atan2(0.0-y,0.0-x);
+	z1 = arr[0];
+	double o3x = x - d5*cos(z1)*sin(y2);
+	double o3y = y - d5*sin(z1)*sin(y2);
+	double o3z = z - d5*cos(y2);
+	printf("o3: %lf %lf %lf\n",o3x,o3y,o3z);
+	//double r = sqrt((o3x - a1*cos(z1))*(o3x - a1*cos(z1)) + (o3y - a1*sin(z1))*(o3y - a1*sin(z1)));
+	double elbow_rad = sqrt(o3x*o3x + o3y*o3y);
+	if (o3x*x < 0.0) elbow_rad *= -1;
+	double r = -elbow_rad - a1;	
 	double s = o3z - d1;
 	double cosq3 = (r*r+s*s - a2*a2 - a3*a3)/(2*a2*a3);
 	//case 1
@@ -254,18 +269,19 @@ int main(int argc, char **argv) {
 	//moveGripper();
 //I am going to add in a command terminal interface that controls the robot with the predefined commands
 
-	double jnt_0,jnt_1,jnt_2,jnt_3,jnt_4;
+
 	jnt_0 = jnt_1 = jnt_2 = jnt_3 = jnt_4 = 0.0;
 	int play, grip;
 	double MOVE_VAL = .01;
+	double ROT_SPD = .05;
 	//movePlatform();
 	moveArm(jnt_0,jnt_1,jnt_2,jnt_3,jnt_4);
 	int cont = 1;
-	double new_joints[5];
-	double x,y,z;
-	double z1, y2, z3;
+	
 	z1 = y2 = z3 = 0.0;
-	x = y = 0.0;
+	y2 = 0.0;
+	x = 0.0;
+	y = 0.0;
 	z = 550.0;
 	while(cont == 1) {
 		char comm = getch();
@@ -395,9 +411,43 @@ int main(int argc, char **argv) {
 				jnt_3 = new_joints[3];
 				jnt_4 = new_joints[4];
 				break;
+			case 'n':
+				y2 += ROT_SPD;
+
+				inverse_kin(new_joints,x,y,z,y2,z3);
+				jnt_0 = new_joints[0];
+				jnt_1 = new_joints[1];
+				jnt_2 = new_joints[2];
+				jnt_3 = new_joints[3];
+				jnt_4 = new_joints[4];
+				break;
+			case 'm':
+				y2 -= ROT_SPD;
+
+				inverse_kin(new_joints,x,y,z,y2,z3);
+				jnt_0 = new_joints[0];
+				jnt_1 = new_joints[1];
+				jnt_2 = new_joints[2];
+				jnt_3 = new_joints[3];
+				jnt_4 = new_joints[4];
+				break;
 			case 'k':
 				//k for kamera
 				get_block_locations();
+				break;
+			case 'l':
+				//list params
+				printf("X = %lf\n",x);
+				printf("Y = %lf\n",y);
+				printf("Z = %lf\n",z);
+				printf("Z1 = %lf\n",z1);
+				printf("Y2 = %lf\n",y2);
+				printf("Z3 = %lf\n",z3);
+				printf("J1 = %lf\n",jnt_0);
+				printf("J2 = %lf\n",jnt_1);
+				printf("J3 = %lf\n",jnt_2);
+				printf("J4 = %lf\n",jnt_3);
+				printf("J5 = %lf\n",jnt_4);
 				break;
 			case '\n':
 				break;
@@ -436,4 +486,94 @@ int main(int argc, char **argv) {
 	ros::shutdown();
 
 	return 0;
+}
+
+void get_block_locations()
+{
+	system("rm -rf /home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc");
+	system("timeout 2 /home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/fakenect-record /home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc > /dev/null");
+	DIR *dir;
+	struct dirent *ent;
+	std::string filename;
+	if ((dir = opendir ("/home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc")) != NULL) {
+	  /* print all the files and directories within directory */
+	  while ((ent = readdir (dir)) != NULL) {
+		if (strstr(ent->d_name,"r")) {	    
+			filename = ent->d_name;
+			break;
+		}
+	  }
+	  closedir (dir);
+	} else {
+	  /* could not open directory */
+	  perror ("");
+	}
+	std::cout << filename << std::endl;
+	std::string path = "/home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc/";
+	std::string filename_path = path.append(filename); //string of filename included with path
+	std::string command = "convert ";
+	std::string output_str = " /home/youbot/libfreenect-inst2/libfreenect/libfreenect/build/bin/dataloc/output.png";
+	command = (command.append(filename_path).append(output_str));
+	const char * c = command.c_str();
+	system(c);
+	//execute vision
+	system("rm -f /home/youbot/cv_util/coords.txt");
+	system("python /home/youbot/cv_util/circles.py > /home/youbot/cv_util/coords.txt");
+
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	fp = fopen("/home/youbot/cv_util/coords.txt", "r");
+	if (fp == NULL) {
+		std::cout << "CV Read Error!"<< std::endl;
+		return;
+	}
+	int num_lin = 0;
+	double coords[2];
+	while ((read = getline(&line, &len, fp)) != -1) {
+		printf("Retrieved line of length %zu :\n", read);
+		coords[num_lin] = std::atoi(line);
+		printf("%lf\n", coords[num_lin]);
+		
+		num_lin++;
+	}
+
+	if (num_lin < 2) {
+		std::cout << "CV Detection Error!" << std::endl;
+		return;
+	}
+	double x_pos = coords[0]/1000000.0;
+	double y_pos = coords[1]/1000000.0;
+	double x_dest = TX_MIN + x_pos*TX_SIZE;
+	double y_dest = TY_MIN + y_pos*TY_SIZE;
+	x_dest -= 8.0*(1.0-x_pos);
+	jnt_0 = jnt_1 = jnt_2 = jnt_3 = jnt_4 = 0.0;
+	jnt_1 = 3.14/2;
+	moveArm(jnt_0,jnt_1,jnt_2,jnt_3,jnt_4);
+	sleep(5);
+	moveGripper(0);
+	sleep(3);
+	inverse_kin(new_joints,x_dest,y_dest,TZ_PERCH,TPITCH,0.0);
+	jnt_0 = new_joints[0];
+	jnt_1 = new_joints[1];
+	jnt_2 = new_joints[2];
+	jnt_3 = new_joints[3];
+	jnt_4 = new_joints[4];
+	moveArm(jnt_0,jnt_1,jnt_2,jnt_3,jnt_4);
+	sleep(5);
+	inverse_kin(new_joints,x_dest,y_dest,TZ_GRAB-(1.0-x_pos)*7,TPITCH,0.0);
+	jnt_0 = new_joints[0];
+	jnt_1 = new_joints[1];
+	jnt_2 = new_joints[2];
+	jnt_3 = new_joints[3];
+	jnt_4 = new_joints[4];
+	moveArm(jnt_0,jnt_1,jnt_2,jnt_3,jnt_4);
+	sleep(5);
+	moveGripper(1);
+	sleep(3);
+	jnt_0 = jnt_1 = jnt_2 = jnt_3 = jnt_4 = 0.0;
+	jnt_1 = 3.14/2;
+	moveArm(jnt_0,jnt_1,jnt_2,jnt_3,jnt_4);
+	sleep(5);
 }
